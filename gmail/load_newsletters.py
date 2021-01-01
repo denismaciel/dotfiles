676952f36cee1921d9bfee925f892ddfc34d8944
Denis Maciel <denispmaciel@gmail.com>
2021-01-01 07:49:21 -0300

    update newsletter filters

---
+++ gmail/load_newsletters.py
@@ -0,0 +1,12 @@
+import yaml
+
+with open('newsletters.yml', 'r') as f:
+    nl = yaml.load(f, Loader=yaml.FullLoader) 
+
+def fmt(email):
+    return f"from:{email}"
+
+current_newsletters = nl['newsletter']
+filter_ = ' OR '.join(fmt(email) for email in current_newsletters)
+
+print(filter_)
