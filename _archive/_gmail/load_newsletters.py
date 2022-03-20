246899abd23a13fd0cf9f6932a7678f31c625faf
Denis Maciel <denispmaciel@gmail.com>
2022-03-20 16:58:33 -0300

    update newsletter management

--- _archive/_gmail/load_newsletters.py
+++ _archive/_gmail/load_newsletters.py
@@ -1,14 +1,17 @@
+import pathlib
 import yaml
-
-with open('newsletters.yml', 'r') as f:
-    nl = yaml.load(f, Loader=yaml.FullLoader)
-
 
 def fmt(email):
     return f'from:{email}'
 
+def main():
+    root = pathlib.Path(__file__).parent
+    with open(root/'newsletters.yml', 'r') as f:
+        nl = yaml.safe_load(f)
 
-current_newsletters = nl['newsletter']
-filter_ = ' OR '.join(fmt(email) for email in current_newsletters)
+    current_newsletters = nl['newsletter']
+    filter_ = ' OR '.join(fmt(email) for email in current_newsletters)
+    print(filter_)
 
-print(filter_)
+if __name__ == "__main__":
+    raise SystemExit(main())
