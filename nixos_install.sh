7dc3bdca9bd6d28de30c8bfffcd46488ceb02582
Denis Maciel <denispmaciel@gmail.com>
2022-07-02 23:23:00 +0200

    nixos manual steps

--- nixos_install.sh
+++ nixos_install.sh
@@ -1,3 +1,19 @@
+nix-env -i git nvim stow
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+
+export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
+nix-shell '<home-manager>' -A install
+
+nix-env -e git nvim stow
+home-manager switch
+
+
+cd ~/dotfiles
+stow nixpkgs
+
+rm -rf ~/.config/nixpkgs
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
