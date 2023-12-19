a1ed7dced2132fee31fd888aa0d0e508383842a7
Denis Maciel <denispmaciel@gmail.com>
2023-12-19 10:06:13 +0100

    open test file automatically

--- nvim/.config/nvim/lua/plugins/test.lua
+++ nvim/.config/nvim/lua/plugins/test.lua
@@ -1,7 +1,40 @@
+local function open_test_file()
+    -- Opens the test file for the current file
+    --
+    -- Get relative path of the current file
+    local current_file_path = vim.fn.expand '%:p'
+    -- remove the root directory from the path
+    current_file_path = string.gsub(current_file_path, vim.fn.getcwd(), '')
+
+    local parts = vim.fn.split(current_file_path, '/')
+    table.remove(parts, 1) -- remove src
+    table.remove(parts, 1) -- remove pkg_name
+
+    -- replace the file name with "_test.py"
+    parts[#parts] = string.gsub(parts[#parts], '.py', '_test.py')
+
+    -- create directory structure if it doesn't exist
+    local test_file_path = './tests'
+    for i = 1, #parts - 1 do
+        test_file_path = test_file_path .. '/' .. parts[i]
+        vim.fn.mkdir(test_file_path, 'p')
+    end
+
+    test_file_path = test_file_path .. '/' .. parts[#parts]
+    vim.cmd('edit ' .. test_file_path)
+end
+
 return {
     {
         'nvim-neotest/neotest',
         keys = {
+            {
+                '<Leader>ro',
+                function()
+                    open_test_file()
+                end,
+                { noremap = true, silent = true },
+            },
             {
                 '<Leader>rt',
                 function()
