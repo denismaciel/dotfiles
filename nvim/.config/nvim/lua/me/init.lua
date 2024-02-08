d344ea552fcd2c928de5ef9629a616ea145722ba
Denis Maciel <denispmaciel@gmail.com>
2024-02-08 06:31:48 +0100

    fix slugify

--- nvim/.config/nvim/lua/me/init.lua
+++ nvim/.config/nvim/lua/me/init.lua
@@ -211,22 +211,5 @@ end
 -- assert(slugify("Already-Has-Hyphens") == "already-has-hyphens", "Test case 10 failed")
 --
 -- print("All test cases passed!")
---
-M.get_visual_selection = function()
-  local function region_to_text(region)
-    local text = ''
-    local maxcol = vim.v.maxcol
-    for line, cols in vim.spairs(region) do
-      local endcol = cols[2] == maxcol and -1 or cols[2]
-      local chunk =
-          vim.api.nvim_buf_get_text(0, line, cols[1], line, endcol, {})[1]
-      text = ('%s%s\n'):format(text, chunk)
-    end
-    return text
-  end
-
-  local r = vim.region(0, '\'<', '\'>', vim.fn.visualmode(), true)
-  return region_to_text(r)
-end
 
 return M
