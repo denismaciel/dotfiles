a3c6fdb79f6f25529c365873f57fa17ac30e4044
Denis Maciel <denispmaciel@gmail.com>
2024-04-21 06:40:58 +0200

    remove harpoon

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -45,69 +45,6 @@ return {
         end,
     },
     {
-        'ThePrimeagen/harpoon',
-        branch = 'harpoon2',
-        config = function()
-            local harpoon = require('harpoon')
-            harpoon:setup()
-            -- basic telescope configuration
-            local conf = require('telescope.config').values
-            local function toggle_telescope(harpoon_files)
-                local file_paths = {}
-                for _, item in ipairs(harpoon_files.items) do
-                    table.insert(file_paths, item.value)
-                end
-
-                require('telescope.pickers')
-                    .new({}, {
-                        prompt_title = 'Harpoon',
-                        finder = require('telescope.finders').new_table({
-                            results = file_paths,
-                        }),
-                        previewer = conf.file_previewer({}),
-                        sorter = conf.generic_sorter({}),
-                    })
-                    :find()
-            end
-
-            vim.keymap.set('n', '<C-e>', function()
-                harpoon.ui:toggle_quick_menu(harpoon:list())
-            end, { desc = 'Open harpoon window' })
-            vim.keymap.set('n', '<C-a>', function()
-                harpoon:list():add()
-            end, { desc = 'Add location to Harpoon list' })
-            -- vim.keymap.set('n', '<C-e>', function()
-            --     harpoon.ui:toggle_quick_menu(harpoon:list())
-            -- end)
-
-            vim.keymap.set('n', '<leader>h', function()
-                harpoon:list():select(1)
-            end)
-            vim.keymap.set('n', '<leader>j', function()
-                harpoon:list():select(2)
-            end)
-            vim.keymap.set('n', '<leader>k', function()
-                harpoon:list():select(3)
-            end)
-            -- vim.keymap.set('n', '<C-s>', function()
-            --     harpoon:list():select(4)
-            -- end)
-
-            vim.keymap.set('n', '<C-P>', function()
-                print('harpoon: previous')
-                harpoon:list():prev()
-            end)
-            vim.keymap.set('n', '<C-N>', function()
-                print('harpoon: next')
-                harpoon:list():next()
-            end)
-        end,
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            'telescope/telescope.nvim',
-        },
-    },
-    {
         { 'akinsho/toggleterm.nvim', version = '*', opts = {} },
         'jackMort/ChatGPT.nvim',
         event = 'VeryLazy',
