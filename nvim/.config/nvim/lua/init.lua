fc683813e33dcfd5bf0c979c50e57bd7abadb1e9
Denis Maciel <denispmaciel@gmail.com>
2024-12-15 12:52:09 +0000

    wip

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -145,6 +145,63 @@ end
 -- ============================
 require('lazy').setup({
     {
+        'mfussenegger/nvim-dap',
+        dependencies = {
+            'leoluz/nvim-dap-go',
+            'rcarriga/nvim-dap-ui',
+            'theHamsta/nvim-dap-virtual-text',
+            'nvim-neotest/nvim-nio',
+            'williamboman/mason.nvim',
+        },
+        config = function()
+            local dap = require('dap')
+            local ui = require('dapui')
+
+            require('dapui').setup()
+            require('dap-go').setup()
+
+            require('nvim-dap-virtual-text').setup({})
+
+            -- Handled by nvim-dap-go
+            -- dap.adapters.go = {
+            --   type = "server",
+            --   port = "${port}",
+            --   executable = {
+            --     command = "dlv",
+            --     args = { "dap", "-l", "127.0.0.1:${port}" },
+            --   },
+            -- }
+
+            vim.keymap.set('n', '<space>b', dap.toggle_breakpoint)
+            vim.keymap.set('n', '<space>gb', dap.run_to_cursor)
+
+            -- Eval var under cursor
+            vim.keymap.set('n', '<space>?', function()
+                require('dapui').eval(nil, { enter = true })
+            end)
+
+            vim.keymap.set('n', '<leader>dc', dap.continue)
+            vim.keymap.set('n', '<leader>dsi', dap.step_into)
+            vim.keymap.set('n', '<leader>dsv', dap.step_over)
+            vim.keymap.set('n', '<leader>dsu', dap.step_out)
+            vim.keymap.set('n', '<leader>dsb', dap.step_back)
+            vim.keymap.set('n', '<leader>dr', dap.restart)
+
+            dap.listeners.before.attach.dapui_config = function()
+                ui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                ui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                ui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                ui.close()
+            end
+        end,
+    },
+    {
         'Pocco81/auto-save.nvim',
         config = function()
             require('auto-save').setup({
