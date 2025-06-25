c4a0302c2b7a895ee3f4d8f1699eb050c54af93b
Denis Maciel <denispmaciel@gmail.com>
2025-06-25 04:30:13 +0100

    master 2025-06-25T04:30:13

--- hosts/ben/configuration.nix
+++ hosts/ben/configuration.nix
@@ -71,16 +71,6 @@
     hostName = "nixos-ben";
     networkmanager.enable = true;
     nameservers = ["127.0.0.1"];
-    extraHosts = ''
-      127.0.0.1 linkedin.com
-      127.0.0.1 www.linkedin.com
-      127.0.0.1 youtube.com
-      127.0.0.1 www.youtube.com
-      127.0.0.1 twitter.com
-      127.0.0.1 www.twitter.com
-      127.0.0.1 x.com
-      127.0.0.1 www.x.com
-    '';
   };
 
   time.timeZone = "Europe/Lisbon";
