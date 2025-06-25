665eece406057c93b44be52a2aec1b314e574124
Denis Maciel <denispmaciel@gmail.com>
2025-06-25 11:14:28 +0100

    master 2025-06-25T11:14:28

--- hosts/ben/configuration.nix
+++ hosts/ben/configuration.nix
@@ -33,7 +33,7 @@
 
   # Trust Tailscale and local network interfaces in firewall
   networking.firewall.enable = true;
-  networking.firewall.trustedInterfaces = ["tailscale0" "enp0s13f0u1u1"];
+  networking.firewall.trustedInterfaces = ["tailscale0" "enp0s20f0u2"];
   hardware.uinput.enable = true;
   users.groups.uinput.members = ["denis"];
   users.groups.input.members = ["denis"];
