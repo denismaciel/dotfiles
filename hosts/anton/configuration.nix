9722c283663d32bab5ed5978481a930be1285129
Denis Maciel <denispmaciel@gmail.com>
2025-08-28 18:17:03 +0100

    master 2025-08-28T18:17:03

--- hosts/anton/configuration.nix
+++ hosts/anton/configuration.nix
@@ -1,19 +1,11 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-{
-  config,
-  pkgs,
-  ...
-}: {
-  services.tailscale.enable = true;
+# Lean server configuration for anton
+{pkgs, ...}: {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    ../../modules/graphics.nix
     ../../modules/unfree.nix
   ];
 
+  # Nix settings
   nix.settings.trusted-users = ["denis"];
   nix.settings.auto-optimise-store = true;
   nix.gc = {
@@ -22,31 +14,27 @@
     options = "--delete-older-than 30d";
   };
 
-  # Bootloader.
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Disable sleep/suspend for server
   systemd.targets.sleep.enable = false;
   systemd.targets.suspend.enable = false;
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
 
-  networking.hostName = "anton"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  # Networking
+  networking.hostName = "anton";
   networking.networkmanager.enable = true;
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [22 443 2222 7422];
+  };
 
-  # Set your time zone.
+  # Localization
   time.timeZone = "Europe/Lisbon";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "pt_PT.UTF-8";
     LC_IDENTIFICATION = "pt_PT.UTF-8";
@@ -59,42 +47,7 @@
     LC_TIME = "pt_PT.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # User configuration
   users.users.denis = {
     isNormalUser = true;
     description = "denis";
@@ -107,57 +60,32 @@
       awscli
       btop
       direnv
-      nodePackages.dotenv-cli
       tmux
       zip
     ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
-  programs.zsh.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # Essential system packages
   environment.systemPackages = with pkgs; [
     neovim
     git
+    wget
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  # System programs
+  programs.zsh.enable = true;
+  security.sudo.wheelNeedsPassword = false;
 
-  # List services that you want to enable:
+  # Services
+  services.tailscale.enable = true;
+  services.openssh = {
+    enable = true;
+    ports = [22 443 2222 7422];
+    settings.PasswordAuthentication = false;
+  };
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.openssh.ports = [22 443 2222 7422];
-  services.openssh.settings.PasswordAuthentication = false;
-
-  # networking = {
-  #   firewall = {
-  #     enable = true;
-  #     allowedTCPPorts = [ 22 2222 ];
-  #   };
-  # };
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  # Virtualization
   virtualisation.docker.enable = true;
+
+  system.stateVersion = "24.05";
 }
